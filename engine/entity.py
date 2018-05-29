131140dfd568ffa909d139847cf1e4795c7259be
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-05-29 11:24:40 +0200

    PNSD-42 PNSD-43 entity is now abstract + codecov

--- engine/entity.py
+++ engine/entity.py
@@ -1,21 +1,12 @@
-from engine.state import State
+import abc
 
 
-class Entity:
+class Entity(abc.ABC):
 
-    def __init__(self, length, transitions):
-        self.length = length
-        self.transitions = transitions
-        self.cells = [State.EMPTY] * length
-
+    @abc.abstractmethod
     def tick(self):
-        next = [0] * self.length
-        for i in range(self.length):
-            next[i] = self.transitions.get([self.get_state(i - 1), self.get_state(i), self.get_state(i + 1)])
-        self.cells = next
+        raise NotImplementedError('users must define tick to use this base class')
 
+    @abc.abstractmethod
     def get_state(self, i):
-        if 0 <= i < self.length:
-            return self.cells[i]
-        else:
-            return State.EMPTY
+        raise NotImplementedError('users must define get_state to use this base class')
--- engine/road.py
+++ engine/road.py
@@ -16,5 +16,19 @@ class Road(Entity):
 
 
     def __init__(self, length):
-        super().__init__(length, self.transitions)
+        self.cells = [State.EMPTY] * length
+        self.length = length
+
+
+    def tick(self):
+        next = [0] * self.length
+        for i in range(self.length):
+            next[i] = self.transitions.get([self.get_state(i-1), self.get_state(i), self.get_state(i+1)])
+        self.cells = next
+
+    def get_state(self, i):
+        if 0 <= i < self.length:
+            return self.cells[i]
+        else:
+            return State.EMPTY
 
