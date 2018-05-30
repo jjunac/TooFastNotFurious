cb807e66fe487a9559169cfec9ad8f1fc666e141
thibaut <thibaut.gonnin@gmail.com>
2018-05-30 17:26:09 +0200

    /PNSD-48-refactor-road-representation-to-graph: Auto stash before merge of "feature/PNSD-48-refactor-road-representation-to-graph" and "origin/feature/PNSD-48-refactor-road-representation-to-graph"
    [PNSD-48] Classic right priority road implemented and tested

---
+++ engine/right_priority_node.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+from engine.traffic_node import TrafficNode
+
+
+class RightPriorityNode(TrafficNode):
+    def __init__(self):
+        TrafficNode.__init__(self)
+        self.priorityMap = {}
+
+    def can_move(self, node):
+        if node in self.priorityMap:
+            return not (self.is_car_present or self.priorityMap.get(node).is_car_present)
+        else:
+            return not self.is_car_present
+
+    # first node has to let the priority to the second
+    def add_priority(self, node1, node2):
+        self.priorityMap[node1] = node2
