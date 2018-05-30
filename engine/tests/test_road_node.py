7c1f165818ab8bb9195bfe2d57079c6305816e30
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-05-30 16:54:41 +0200

    PNSD-48 add util module

--- engine/tests/test_road_node.py
+++ engine/tests/test_road_node.py
@@ -1,31 +1,13 @@
 import unittest
 
 from engine.road_node import RoadNode
+from engine.utils import *
 
 
 class MyTestCase(unittest.TestCase):
 
-    def build_road(self, length):
-        res = [RoadNode()]
-        for i in range(length-1):
-            res.append(RoadNode())
-            res[-2].successors.append(res[-1])
-            res[-1].predecessors.append(res[-2])
-        return res
-
-
-    def compute_next(self, road):
-        for n in road:
-            n.compute_next()
-
-
-    def apply_next(self, road):
-        for n in road:
-            n.apply_next()
-
-
     def test_a_car_should_go_forward_when_it_is_alone(self):
-        road = self.build_road(10)
+        road = build_road(10)
         for n in road:
             self.assertFalse(n.is_car_present)
         road[0].is_car_present = True
@@ -38,68 +20,68 @@ class MyTestCase(unittest.TestCase):
             # Empty after the car
             for j in range(i+1, len(road)):
                 self.assertFalse(road[j].is_car_present)
-            self.compute_next(road)
-            self.apply_next(road)
+            compute_next(road)
+            apply_next(road)
 
 
     def test_a_car_should_stop_when_there_is_a_car_ahead(self):
-        road = self.build_road(10)
+        road = build_road(10)
         for c in road:
             self.assertFalse(False, c.is_car_present)
         for i in range(4, 10):
             road[i].is_car_present = True
         road[1].is_car_present = True
 
-        self.compute_next(road)
-        self.apply_next(road)
+        compute_next(road)
+        apply_next(road)
         self.assertFalse(road[1].is_car_present)
         self.assertTrue(road[2].is_car_present)
         self.assertFalse(road[3].is_car_present)
 
-        self.compute_next(road)
-        self.apply_next(road)
+        compute_next(road)
+        apply_next(road)
         self.assertFalse(road[2].is_car_present)
         self.assertTrue(road[3].is_car_present)
         self.assertTrue(road[4].is_car_present)
 
-        self.compute_next(road)
-        self.apply_next(road)
+        compute_next(road)
+        apply_next(road)
         self.assertFalse(road[2].is_car_present)
         self.assertTrue(road[3].is_car_present)
         self.assertTrue(road[4].is_car_present)
 
     def test_a_car_should_wait_before_go_forward_when_there_is_a_car_ahead(self):
-        road = self.build_road(3)
+        road = build_road(3)
         road[0].is_car_present = True
         road[1].is_car_present = True
         road[2].is_car_present = True
 
-        self.compute_next(road)
-        self.apply_next(road)
+        compute_next(road)
+        apply_next(road)
         self.assertTrue(road[0].is_car_present)
         self.assertTrue(road[1].is_car_present)
         self.assertFalse(road[2].is_car_present)
 
-        self.compute_next(road)
-        self.apply_next(road)
+        compute_next(road)
+        apply_next(road)
         self.assertTrue(road[0].is_car_present)
         self.assertFalse(road[1].is_car_present)
         self.assertTrue(road[2].is_car_present)
 
-        self.compute_next(road)
-        self.apply_next(road)
+        compute_next(road)
+        apply_next(road)
         self.assertFalse(road[0].is_car_present)
         self.assertTrue(road[1].is_car_present)
         self.assertFalse(road[2].is_car_present)
 
-        self.compute_next(road)
-        self.apply_next(road)
+        compute_next(road)
+        apply_next(road)
         self.assertFalse(road[0].is_car_present)
         self.assertFalse(road[1].is_car_present)
         self.assertTrue(road[2].is_car_present)
 
-        self.compute_next(road)
-        self.apply_next(road)
+        compute_next(road)
+        apply_next(road)
         self.assertFalse(road[0].is_car_present)
         self.assertFalse(road[1].is_car_present)
         self.assertFalse(road[2].is_car_present)
