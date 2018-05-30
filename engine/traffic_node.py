47dbb0b8d7334514d3fca32a6750439cc1c27a7c
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-05-30 15:34:50 +0200

    PSND-48 improve node class

--- engine/traffic_node.py
+++ engine/traffic_node.py
@@ -6,7 +6,17 @@ class TrafficNode(ABC):
         self.successors = []
         self.predecessors = []
         self.is_car_present = False
+        self.next_is_car_present = False
 
     @abstractmethod
     def can_move(self, node):
         pass
+
+    def compute_next(self):
+        # FIXME always send to the first successor
+        if self.is_car_present and self.successors[0].can_move(self):
+            self.next_is_car_present = False
+            self.successors[0].next_is_car_present = True
+
+    def apply_next(self):
+        self.is_car_present = self.next_is_car_present
