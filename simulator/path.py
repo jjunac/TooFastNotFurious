22a30157297e3992c3733e53b57c3e2bfdddb3ff
thibaut <thibaut.gonnin@gmail.com>
2018-05-31 10:25:29 +0200

    /PNSD-48-refactor-road-representation-to-graph: /PNSD-48-refactor-road-representation-to-graph: Auto stash before merge of "feature/PNSD-48-refactor-road-representation-to-graph" and "origin/feature/PNSD-48-refactor-road-representation-to-graph"
    PNSD-48 Génrération d'une classe Path

---
+++ simulator/path.py
@@ -0,0 +1,10 @@
+
+class Path:
+
+    def __init__(self):
+        self.queue = []
+
+    #récup le prochain indice pour le successeur. Dans le cas où on a fini et on est pas arrivé à la fin on return 0
+
+    def get_new_index(self):
+        return 0
