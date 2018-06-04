8a34c53579648b66e8f665d80e8c8559207696be
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-06-04 10:13:14 +0200

    add exemples

---
+++ examples/right_priority_1_entry_2_exit.py
@@ -0,0 +1,27 @@
+from modeler import *
+from shared import Orientation
+
+
+def main():
+    s = new_simulation()
+
+    entry1 = entry_node().with_rate(0.2)
+    s.add_node(entry1)
+    exit1 = exit_node()
+    s.add_node(exit1)
+    exit2 = exit_node()
+    s.add_node(exit2)
+    junction = right_priority_junction()
+    s.add_node(junction)
+
+    s.add_road(entry1.connect(Orientation.NORTH).to(junction).with_length(7))
+    s.add_road(junction.connect(Orientation.NORTH).to(exit1).with_length(7))
+    s.add_road(junction.connect(Orientation.WEST).to(exit2).with_length(4))
+
+    s.add_path(entry1.go_through(junction, exit1).with_proportion(30))
+    s.add_path(entry1.go_through(junction, exit2).with_proportion(70))
+
+    s.run_graphical_for(400)
+
+if __name__ == '__main__':
+    main()
