5545392ecf4b67c95d8072ae4bd562f196e16688
Olivier Boulet <olivier.boulet@etu.unice.fr>
2018-06-04 22:21:10 +0200

    PNSD-13 Solving little issue

--- simulator/tests/test_statistics.py
+++ simulator/tests/test_statistics.py
@@ -21,20 +21,19 @@ class TestStatistics(unittest.TestCase):
         self.assertFalse(p in stat.list_time_travel)
         self.assertFalse(p2 in stat.list_time_travel)
 
-        stat.add_travel_time(p, 100)
-        stat.add_travel_time(p, 200)
+        stat.add_travel_time(entry1, p, 100)
+        stat.add_travel_time(entry1, p, 200)
 
-        stat.add_travel_time(p2, 600)
-        stat.add_travel_time(p2, 500)
-        stat.add_travel_time(p2, 800)
+        stat.add_travel_time(entry1, p2, 600)
+        stat.add_travel_time(entry1, p2, 500)
+        stat.add_travel_time(entry1, p2, 800)
 
-        self.assertEqual(100, stat.list_time_travel[p][0])
-        self.assertEqual(200, stat.list_time_travel[p][1])
+        self.assertEqual(100, stat.list_time_travel[(entry1, p)][0])
+        self.assertEqual(200, stat.list_time_travel[(entry1, p)][1])
 
-        self.assertEqual(600, stat.list_time_travel[p2][0])
-        self.assertEqual(500, stat.list_time_travel[p2][1])
-        self.assertEqual(800, stat.list_time_travel[p2][2])
+        self.assertEqual(600, stat.list_time_travel[(entry1, p2)][0])
+        self.assertEqual(500, stat.list_time_travel[(entry1, p2)][1])
+        self.assertEqual(800, stat.list_time_travel[(entry1, p2)][2])
 
-        self.assertEqual(2, len(stat.list_time_travel[p]))
-        self.assertEqual(3, len(stat.list_time_travel[p2]))
-
+        self.assertEqual(2, len(stat.list_time_travel[(entry1, p)]))
+        self.assertEqual(3, len(stat.list_time_travel[(entry1, p2)]))
