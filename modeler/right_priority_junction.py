6e5501e444a5cea7614364ee8aa68d16394c6bf0
thibaut <thibaut.gonnin@gmail.com>
2018-06-06 19:50:41 +0200

    PNSD-54 modeler now use the new junction constructor

--- modeler/right_priority_junction.py
+++ modeler/right_priority_junction.py
@@ -10,19 +10,19 @@ class RightPriorityJunction(Node):
         super().__init__()
 
     def build(self, sim):
-        n_way_north = self.exits.get(Orientation.NORTH, Road(None, Orientation.NORTH).with_n_ways(0)).n_ways + \
-                      self.entries.get(Orientation.SOUTH, Road(None, Orientation.SOUTH).with_n_ways(0)).n_ways
-
-        n_way_south = self.exits.get(Orientation.SOUTH, Road(None, Orientation.SOUTH).with_n_ways(0)).n_ways + \
-                      self.entries.get(Orientation.NORTH, Road(None, Orientation.NORTH).with_n_ways(0)).n_ways
-
-        n_way_east = self.exits.get(Orientation.EAST, Road(None, Orientation.EAST).with_n_ways(0)).n_ways + \
-                     self.entries.get(Orientation.WEST, Road(None, Orientation.WEST).with_n_ways(0)).n_ways
-
-        n_way_west = self.exits.get(Orientation.WEST, Road(None, Orientation.WEST).with_n_ways(0)).n_ways + \
-                     self.entries.get(Orientation.EAST, Road(None, Orientation.EAST).with_n_ways(0)).n_ways
-
-        size_north_south = max(n_way_north, n_way_south)
-        size_east_west = max(n_way_east, n_way_west)
-
-        return simulator.RightPriorityJunction(sim, size_north_south, size_east_west)
+        io_roads = {}
+        i_north = 1
+        o_south = 1
+        o_west = 1
+        i_east = 1
+        if Orientation.NORTH in self.entries:
+            i_north = self.entries[Orientation.NORTH].n_ways
+        if Orientation.SOUTH in self.exits:
+            o_south = self.exits[Orientation.SOUTH].n_ways
+        if Orientation.EAST in self.entries:
+            i_east = self.entries[Orientation.EAST].n_ways
+        if Orientation.WEST in self.exits:
+            o_west = self.exits[Orientation.WEST].n_ways
+        io_roads[Orientation.NORTH] = (i_north, o_south)
+        io_roads[Orientation.EAST] = (i_east, o_west)
+        return simulator.RightPriorityJunction(sim, io_roads)
