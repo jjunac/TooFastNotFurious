8cc94007d1cecce9de295bc48fb2519c0b941df3
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-06-07 21:48:48 +0200

    PNSD-54 change the orientation system to fix some junction cases

--- simulator/right_priority_junction.py
+++ simulator/right_priority_junction.py
@@ -17,18 +17,20 @@ class RightPriorityJunction(AbstractEntity):
         self.__link_nodes()
 
     def do_add_predecessor(self, orientation, predecessor):
-        end = predecessor.get_end(orientation)
-        start = self.get_start(orientation)
+        end = predecessor.get_end(orientation.invert())
+        start = self.get_start(orientation.invert())
         for i in range(len(start)):
             link(end[i], start[i])
             self.simulator.dependencies[(end[i], start[i])] = [start[i]]
-        if orientation.left() in self.predecessors:
-            for i in range(len(start)):
-                self.simulator.dependencies[(end[i], start[i])].extend(self.get_end_of_predecessor(orientation.left()))
+        # The car on the left (so heading right) needs to leave the priority
         if orientation.right() in self.predecessors:
-            end_of_predecessor = self.get_end_of_predecessor(orientation.right())
+            for i in range(len(start)):
+                self.simulator.dependencies[(end[i], start[i])].extend(self.get_end_of_predecessor(orientation.right()))
+        # This car needs to leave the priority to the car on the right (so heading left)
+        if orientation.left() in self.predecessors:
+            end_of_predecessor = self.get_end_of_predecessor(orientation.left())
             for i in range(len(end_of_predecessor)):
-                self.simulator.dependencies[(end_of_predecessor[i], self.get_start(orientation.right())[i])].extend(end)
+                self.simulator.dependencies[(end_of_predecessor[i], self.get_start(orientation.left())[i])].extend(end)
 
     def get_end_of_predecessor(self, orientation):
         return self.predecessors[orientation].get_end(orientation)
