d0f767ae917a1a01033e8c672ee3a2e5a7b8ae98
Olivier Boulet <olivier.boulet@etu.unice.fr>
2018-06-08 09:27:35 +0200

    PNSD-13 Test with simple stop on T junction

--- simulator/stop_junction.py
+++ simulator/stop_junction.py
@@ -15,15 +15,14 @@ class StopJunction(RightPriorityJunction):
         for i in range(len(start)):
             link(end[i], start[i])
             self.simulator.dependencies[(end[i], start[i])] = [start[i]]
-        # The car on the left (so heading right) needs to leave the priority
-        if orientation.right() in self.predecessors:
+        # This car needs to leave the priority to the car on the left (so heading right)
+        if orientation.right() in self.predecessors and self.io_roads[orientation][2] == True:
             end_of_predecessor = self.get_end_of_predecessor(orientation.right())
             for i in range(len(end_of_predecessor)):
                 self.simulator.dependencies[(end_of_predecessor[i], self.get_start(orientation.right())[i])].extend(end)
         # This car needs to leave the priority to the car on the right (so heading left)
-        if orientation.left() in self.predecessors:
+        if orientation.left() in self.predecessors and self.io_roads[orientation][2] == True:
             end_of_predecessor = self.get_end_of_predecessor(orientation.left())
             print(end_of_predecessor)
             for i in range(len(end_of_predecessor)):
                 self.simulator.dependencies[(end_of_predecessor[i], self.get_start(orientation.left())[i])].extend(end)
-
