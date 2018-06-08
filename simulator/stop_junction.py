5b787a2d5b3cf0f4f3771bd5c7b97a276215bfcb
Olivier Boulet <olivier.boulet@etu.unice.fr>
2018-06-08 13:45:05 +0200

    PNSD-13 Orientation for the stop

--- simulator/stop_junction.py
+++ simulator/stop_junction.py
@@ -6,23 +6,40 @@ from simulator.utils import link
 
 class StopJunction(RightPriorityJunction):
 
-    def __init__(self, simulator, io_roads):
+    def __init__(self, simulator, io_roads, orientation_stop):
+        self.orientation_stop = orientation_stop
         super().__init__(simulator, io_roads)
 
+    def add_predecessor(self, orientation, predecessor):
+        self.predecessors[orientation.invert()] = predecessor
+        predecessor.successors[orientation] = self
+        self.do_add_predecessor(orientation, predecessor)
+
     def do_add_predecessor(self, orientation, predecessor):
+        # print("coucou", orientation.right())
+        # print("coucou", self.predecessors)
+        # print("coucou", self.io_roads[orientation.right()])
+
         end = predecessor.get_end(orientation.invert())
         start = self.get_start(orientation.invert())
         for i in range(len(start)):
             link(end[i], start[i])
             self.simulator.dependencies[(end[i], start[i])] = [start[i]]
         # This car needs to leave the priority to the car on the left (so heading right)
-        if orientation.right() in self.predecessors and self.io_roads[orientation][2] == True:
-            end_of_predecessor = self.get_end_of_predecessor(orientation.right())
-            for i in range(len(end_of_predecessor)):
-                self.simulator.dependencies[(end_of_predecessor[i], self.get_start(orientation.right())[i])].extend(end)
-        # This car needs to leave the priority to the car on the right (so heading left)
-        if orientation.left() in self.predecessors and self.io_roads[orientation][2] == True:
-            end_of_predecessor = self.get_end_of_predecessor(orientation.left())
-            print(end_of_predecessor)
-            for i in range(len(end_of_predecessor)):
-                self.simulator.dependencies[(end_of_predecessor[i], self.get_start(orientation.left())[i])].extend(end)
+
+        # print(self.predecessors[Orientation.SOUTH])
+
+        if orientation in self.orientation_stop_list:
+            print("STOP")
+            if orientation.right() in self.predecessors:
+                for i in range(len(start)):
+                    self.simulator.dependencies[(end[i], start[i])].extend(
+                        self.get_end_of_predecessor(orientation.right()))
+
+            if orientation.left() in self.predecessors:
+                for i in range(len(start)):
+                    self.simulator.dependencies[(end[i], start[i])].extend(
+                        self.get_end_of_predecessor(orientation.left()))
+
+
+
--- simulator/tests/test_stop_junction.py
+++ simulator/tests/test_stop_junction.py
@@ -11,10 +11,10 @@ from copy import deepcopy
 
 class TestStopJunction(unittest.TestCase):
 
-    def test_should_go_when_there_is_no_right_priority(self):
+    def test_car_should_stop_from_west(self):
         simulator = Simulator()
-        rs = StopJunction(simulator, {Orientation.NORTH: (0, 1, False), Orientation.EAST: (1, 0, False),
-                                      Orientation.SOUTH: (1, 0, True), Orientation.WEST: (0, 1, False)})
+        rs = StopJunction(simulator, {Orientation.NORTH: (0, 1), Orientation.EAST: (1, 0),
+                                      Orientation.SOUTH: (1, 0), Orientation.WEST: (0, 1)})
         r1 = Road(simulator, 1, Orientation.NORTH, 1)
         r2 = Road(simulator, 1, Orientation.WEST, 1)
         r3 = Road(simulator, 1, Orientation.WEST, 1)
@@ -24,6 +24,59 @@ class TestStopJunction(unittest.TestCase):
         rs.add_predecessor(Orientation.NORTH, r1)
         rs.add_predecessor(Orientation.WEST, r2)
         r3.add_predecessor(Orientation.WEST, rs)
+
+        print("Node", r1.nodes, r2.nodes, r3.nodes)
+        print("JUNCTION", rs.nodes)
+        self.assertIsNotNone(r1.nodes[0][0].current_car)
+        self.assertIsNotNone(r2.nodes[0][0].current_car)
+        self.assertIsNone(rs.nodes[0][0].current_car)
+        self.assertIsNone(r3.nodes[0][0].current_car)
+
+        simulator.tick()
+        self.assertIsNone(r1.nodes[0][0].current_car)
+        self.assertIsNotNone(r2.nodes[0][0].current_car)
+        self.assertIsNotNone(rs.nodes[0][0].current_car)
+        self.assertIsNone(r3.nodes[0][0].current_car)
+        r1.nodes[0][0].current_car = Car(Path([rs.nodes[0][0], r3.nodes[0][0]]), r1.nodes[0][0])
+        self.assertIsNotNone(r1.nodes[0][0].current_car)
+
+        simulator.tick()
+        self.assertIsNotNone(r1.nodes[0][0].current_car)
+        self.assertIsNotNone(r2.nodes[0][0].current_car)
+        self.assertIsNone(rs.nodes[0][0].current_car)
+        self.assertIsNotNone(r3.nodes[0][0].current_car)
+
+        simulator.tick()
+        self.assertIsNone(r1.nodes[0][0].current_car)
+        self.assertIsNotNone(r2.nodes[0][0].current_car)
+        self.assertIsNotNone(rs.nodes[0][0].current_car)
+        self.assertIsNone(r3.nodes[0][0].current_car)
+
+        simulator.tick()
+        self.assertIsNone(r1.nodes[0][0].current_car)
+        self.assertIsNotNone(r2.nodes[0][0].current_car)
+        self.assertIsNone(rs.nodes[0][0].current_car)
+        self.assertIsNotNone(r3.nodes[0][0].current_car)
+
+        simulator.tick()
+        self.assertIsNone(r1.nodes[0][0].current_car)
+        self.assertIsNone(r2.nodes[0][0].current_car)
+        self.assertIsNotNone(rs.nodes[0][0].current_car)
+        self.assertIsNone(r3.nodes[0][0].current_car)
+
+    def test_car_should_stop_from_east(self):
+        simulator = Simulator()
+        rs = StopJunction(simulator, {Orientation.NORTH: (0, 1, False), Orientation.WEST: (1, 0, True),
+                                      Orientation.SOUTH: (1, 0, False), Orientation.EAST: (0, 1, False)})
+        r1 = Road(simulator, 1, Orientation.NORTH, 1)
+        r2 = Road(simulator, 1, Orientation.EAST, 1)
+        r3 = Road(simulator, 1, Orientation.EAST, 1)
+        r1.nodes[0][0].current_car = Car(Path([rs.nodes[0][0], r3.nodes[0][0]]), r1.nodes[0][0])
+        r2.nodes[0][0].current_car = Car(Path([rs.nodes[0][0], r3.nodes[0][0]]), r2.nodes[0][0])
+
+        rs.add_predecessor(Orientation.NORTH, r1)
+        rs.add_predecessor(Orientation.EAST, r2)
+        r3.add_predecessor(Orientation.EAST, rs)
 
         self.assertIsNotNone(r1.nodes[0][0].current_car)
         self.assertIsNotNone(r2.nodes[0][0].current_car)
