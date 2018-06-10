fe04d60e947a4ee847f62be3882395e06617580c
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-06-10 11:33:17 +0200

    PNSD-62 add stop to the modeler

---
+++ modeler/stop_junction.py
@@ -0,0 +1,15 @@
+import simulator
+from modeler.junction_node import JunctionNode
+
+
+class StopJunction(JunctionNode):
+
+    def __init__(self):
+        super().__init__()
+        self.stop_orientation = None
+
+    def with_stop_on_road(self, orientation):
+        self.stop_orientation = orientation
+
+    def build(self, sim):
+        return simulator.StopJunction(sim, self.get_io_roads(), self.stop_orientation)
--- simulator/stop_junction.py
+++ simulator/stop_junction.py
@@ -3,24 +3,24 @@ from simulator.junction import Junction
 
 class StopJunction(Junction):
 
-    def __init__(self, simulator, io_roads, orientation_stop):
+    def __init__(self, simulator, io_roads, stop_orientation):
         super().__init__(simulator, io_roads)
-        self.orientation_stop = orientation_stop
+        self.stop_orientation = stop_orientation
 
     def do_add_predecessor(self, orientation, predecessor):
         super().do_add_predecessor(orientation, predecessor)
         end = predecessor.get_end(orientation.invert())
         start = self.get_start(orientation.invert())
 
-        if orientation == self.orientation_stop:
+        if orientation == self.stop_orientation:
             for o in self.predecessors.keys():
                 if o == orientation.invert():
                     continue
                 for i in range(len(start)):
                     self.simulator.dependencies[(end[i], start[i])].extend(self.get_end_of_predecessor(o))
         else:
-            if self.orientation_stop.invert() in self.predecessors:
-                end_of_predecessor = self.get_end_of_predecessor(self.orientation_stop.invert())
-                stop_start = self.get_start(self.orientation_stop.invert())
+            if self.stop_orientation.invert() in self.predecessors:
+                end_of_predecessor = self.get_end_of_predecessor(self.stop_orientation.invert())
+                stop_start = self.get_start(self.stop_orientation.invert())
                 for i in range(len(end_of_predecessor)):
                     self.simulator.dependencies[(end_of_predecessor[i], stop_start[i])].extend(end)
