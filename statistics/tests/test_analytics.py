b7eb93113cb4d02f3c1712a477642066274b77f0
Olivier Boulet <olivier.boulet@etu.unice.fr>
2018-06-13 00:03:33 +0200

    PNSD-69 New functions tested

--- statistics/tests/test_analytics.py
+++ statistics/tests/test_analytics.py
@@ -351,6 +351,109 @@ class TestAnalytics(unittest.TestCase):
         self.assertEqual(10, res[car4])
         self.assertEqual(20, res[car5])
 
+    def test_should_compute_the_delay_time_expectancy_with_traffic_load(self):
+        s = Simulator()
+        entry1 = Entry(s, 0, 1)
+        entry2 = Entry(s, 0, 1)
+        entry3 = Entry(s, 0, 1)
+
+        p1 = Path([])
+        p2 = Path([])
+
+        exit1 = Exit(s, 1)
+        exit2 = Exit(s, 1)
+
+        road1 = Road(s, 100, Orientation.SOUTH, 1)
+
+        car1 = Car(p2, entry1, 0)
+        car2 = Car(p2, entry2, 0)
+        car3 = Car(p2, entry3, 0)
+        car4 = Car(p2, entry1, 0)
+        car5 = Car(p2, entry2, 0)
+        car6 = Car(p2, entry3, 0)
+
+        nodes = []
+
+        for i in range(0, 10):
+            nodes.append(road1.nodes[0][i])
+
+        car1.visited_nodes = nodes
+        car6.visited_nodes = nodes
+        nodes1 = []
+
+        for i in range(0, 20):
+            nodes1.append(road1.nodes[0][i])
+
+        car2.visited_nodes = nodes1
+        car4.visited_nodes = nodes1
+        nodes2 = []
+
+        for i in range(0, 30):
+            nodes2.append(road1.nodes[0][i])
+
+        car3.visited_nodes = nodes2
+        car5.visited_nodes = nodes2
+
+        car1.original_path.nodes = nodes
+        car2.original_path.nodes = nodes
+        car3.original_path.nodes = nodes
+        car4.original_path.nodes = nodes
+        car5.original_path.nodes = nodes
+        car6.original_path.nodes = nodes
+
+        car1.departure_tick = 1
+        car2.departure_tick = 1
+        car3.departure_tick = 2
+        car4.departure_tick = 3
+        car5.departure_tick = 3
+        car6.departure_tick = 4
+
+        stats = {exit1: {(entry1, p1): [car1, car2, car3, car6]},
+                 exit2: {(entry2, p1): [car4, car5]}}
+
+        t_load = []
+
+        for i in range(0, 50):
+            t_load.append(0)
+
+        t_load[1] = 2
+        t_load[2] = 3
+        t_load[3] = 5
+        t_load[4] = 6
+
+        for i in range(5, 11):
+            t_load[i] = 6
+
+        for i in range(11, 15):
+            t_load[i] = 5
+
+        for i in range(15, 21):
+            t_load[i] = 4
+
+        for i in range(21, 24):
+            t_load[i] = 3
+
+        for i in range(24, 32):
+            t_load[i] = 2
+
+        t_load[32] = 1
+        t_load[33] = 1
+
+        a = Analytics([], t_load)
+
+        delay = a.compute_delay_time_by_car(stats)
+
+        res = a.compute_delay_time_expectancy_with_traffic_load(delay)
+
+        self.assertEqual(100, res[1])
+        self.assertEqual(250, res[2])
+        self.assertEqual(250, res[8])
+        self.assertAlmostEqual(300, res[22], delta=0.1)
+        self.assertEqual(400, res[28])
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
