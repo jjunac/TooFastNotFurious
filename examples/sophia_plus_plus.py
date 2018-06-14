f82c012d968066d784852d1bcda6954da5ce3a1b
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-06-14 23:28:26 +0200

    add bench

--- examples/sophia_plus_plus.py
+++ examples/sophia_plus_plus.py
@@ -1,3 +1,5 @@
+import time
+
 from modeler import *
 from shared import Orientation
 
@@ -36,25 +38,25 @@ def main():
     s.add_node(feu_rouge1)
 
 
-    s.add_road(carrefour_in.connect(Orientation.WEST).to(rondpoint1).with_length(2).with_n_ways(2))
-    s.add_road(rondpoint1.connect(Orientation.EAST).to(carrefour_out).with_length(2).with_n_ways(2))
+    s.add_road(carrefour_in.connect(Orientation.WEST).to(rondpoint1).with_length(400).with_n_ways(2))
+    s.add_road(rondpoint1.connect(Orientation.EAST).to(carrefour_out).with_length(400).with_n_ways(2))
 
-    s.add_road(sophia_in.connect(Orientation.SOUTH).to(rondpoint1).with_length(2).with_n_ways(2))
-    s.add_road(rondpoint1.connect(Orientation.NORTH).to(sophia_out).with_length(2).with_n_ways(2))
+    s.add_road(sophia_in.connect(Orientation.SOUTH).to(rondpoint1).with_length(400).with_n_ways(2))
+    s.add_road(rondpoint1.connect(Orientation.NORTH).to(sophia_out).with_length(400).with_n_ways(2))
 
-    s.add_road(juan_in.connect(Orientation.NORTH).to(rondpoint1).with_length(2).with_n_ways(2))
-    s.add_road(rondpoint1.connect(Orientation.SOUTH).to(juan_out).with_length(2).with_n_ways(2))
-
-
-    s.add_road(feu_rouge1.connect(Orientation.EAST).to(rondpoint1).with_length(2).with_n_ways(2))
-    s.add_road(rondpoint1.connect(Orientation.WEST).to(feu_rouge1).with_length(2).with_n_ways(2))
+    s.add_road(juan_in.connect(Orientation.NORTH).to(rondpoint1).with_length(400).with_n_ways(2))
+    s.add_road(rondpoint1.connect(Orientation.SOUTH).to(juan_out).with_length(400).with_n_ways(2))
 
 
-    s.add_road(entry1.connect(Orientation.SOUTH).to(feu_rouge1).with_length(2).with_n_ways(2))
-    s.add_road(feu_rouge1.connect(Orientation.NORTH).to(exit1).with_length(2).with_n_ways(2))
+    s.add_road(feu_rouge1.connect(Orientation.EAST).to(rondpoint1).with_length(400).with_n_ways(2))
+    s.add_road(rondpoint1.connect(Orientation.WEST).to(feu_rouge1).with_length(400).with_n_ways(2))
 
-    s.add_road(entry2.connect(Orientation.NORTH).to(feu_rouge1).with_length(2).with_n_ways(2))
-    s.add_road(feu_rouge1.connect(Orientation.SOUTH).to(exit2).with_length(2).with_n_ways(2))
+
+    s.add_road(entry1.connect(Orientation.SOUTH).to(feu_rouge1).with_length(400).with_n_ways(2))
+    s.add_road(feu_rouge1.connect(Orientation.NORTH).to(exit1).with_length(400).with_n_ways(2))
+
+    s.add_road(entry2.connect(Orientation.NORTH).to(feu_rouge1).with_length(400).with_n_ways(2))
+    s.add_road(feu_rouge1.connect(Orientation.SOUTH).to(exit2).with_length(400).with_n_ways(2))
 
 
     s.add_path(carrefour_in.to(sophia_out).with_proportion(30))
@@ -69,7 +71,10 @@ def main():
     s.add_path(juan_in.to(carrefour_out).with_proportion(30))
     s.add_path(juan_in.to(exit1).with_proportion(40))
 
-    s.run_graphical_for(400)
+    start = time.time()
+    s.run_for(1800)
+    print(len(s.simulator.get_nodes()))
+    print(time.time() - start)
 
 
 if __name__ == '__main__':
